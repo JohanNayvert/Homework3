e78627753d5395b0a7e75285bfaf003d1ac8a917
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-09-29 23:17:35 +0500

    Task_6-9 Complete

--- com.skypro/src/Main.java
+++ com.skypro/src/Main.java
@@ -8,6 +8,9 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
+        task8();
+        task9();
 
         System.out.println("Homework_3 Complete");
     }
@@ -99,6 +102,56 @@ public class Main {
             System.out.println("Only standing place!");
         } else {
             System.out.println("No Place!");
+        }
+        System.out.println(" ");
+    }
+
+    //task7
+    public static void task7() {
+        System.out.println("Task_7");
+        int age = 22;
+        if (age>=2&&age<=6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        if (age>=7&&age<=18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+        if (age>18&&age<24) {
+            System.out.println("Если возраст человека равен " + age + ", то его место в университете");
+        } else if (age>=24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходитьна работу");
+        }
+        System.out.println(" ");
+    }
+
+    //task8
+    public static void task8() {
+        System.out.println("Task_8");
+        int age = 3;
+        if (age<5) {
+            System.out.println("Если ребенку " + age + " лет, он не может кататься на атракционе");
+        }
+        if (age>=5&&age<14) {
+            System.out.println("Если ребенку " + age + " лет он может кататься на атракционе в сопровождении взрослого");
+        } else if (age>14) {
+            System.out.println("Если ребенку " + age + " лет он может кататься на атракционе без сопровождения взрослого");
+        }
+        System.out.println(" ");
+    }
+
+    //task9
+    public static void task9() {
+        System.out.println("Task_9");
+        int one = 1;
+        int two = 2;
+        int free = 3;
+        if (one>two&&one>free) {
+            System.out.println(one);
+        }
+        if (two>one&&two>free) {
+            System.out.println(two);
+        }else{
+            System.out.println("Наибольшее число: " + free);
         }
         System.out.println(" ");
     }
